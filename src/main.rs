c06b9c80db88044a02ceb183660f959c0f030b75
Bruno Medeiros <bruno.do.medeiros@gmail.com>
2016-12-02 14:36:52 +0000

    some renames.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Copyright 2016 Bruno Medeiros
+//
+// Licensed under the Apache License, Version 2.0 
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0>. 
+// This file may not be copied, modified, or distributed
+// except according to those terms.
 
 #[macro_use] extern crate log;
 extern crate env_logger;
